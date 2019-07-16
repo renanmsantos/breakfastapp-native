8bc866e9a49b14517fa91e0fb1de2a1bfd9b69c3
Renan Moreira Santos <renanmoreirasan@gmail.com>
2019-07-16 01:11:33 -0300

    Initialize first view.

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#FEC810', '#FF5F49'],
+})`
+  flex: 1;
+`;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+import Background from '~/components/Background';
 
-export default function SignIn() {
-  return <View />;
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={() => {}}>Fazer login</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar nova conta</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
