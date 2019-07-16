340cfa63695f60273eb89e7e5ede872e1fe8c42f
Renan Moreira Santos <renanmoreirasan@gmail.com>
2019-07-15 23:21:00 -0300

    Base code.

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,4 +42,11 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
